1486c631cbd414841d98d8e3c827c1fa209e0cf6
Shane Kennedy <shane.kennedy19@gmail.com>
2018-03-20 14:11:41 -0400

    fix scenario activity

--- app/src/main/java/com/example/groupb/sack_app/ResourcesActivity.java
+++ app/src/main/java/com/example/groupb/sack_app/ResourcesActivity.java
@@ -1,13 +1,35 @@
 package com.example.groupb.sack_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class ResourcesActivity extends AppCompatActivity {
+public class ScenariosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_resources);
+        setContentView(R.layout.activity_scenarios);
+    }
+
+    public void toScenario1(View view) {
+        Intent intent = new Intent(this, scenario1Main.class);
+        startActivity(intent);
+    }
+
+    public void toScenario2(View view) {
+        Intent intent = new Intent(this, scenario2Main.class);
+        startActivity(intent);
+    }
+
+    public void toScenario3(View view) {
+        Intent intent = new Intent(this, scenario3Main.class);
+        startActivity(intent);
+    }
+
+    public void toScenario4(View view) {
+        Intent intent = new Intent(this, scenario4Main.class);
+        startActivity(intent);
     }
 }
