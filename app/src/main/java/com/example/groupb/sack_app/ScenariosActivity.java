862a22010dc00085da8aac9814df3fe83773642a
Shane Kennedy <shane.kennedy19@gmail.com>
2018-03-20 14:49:47 -0400

    accidentally added scenario code to the wrong activity, this is now fixed.

--- app/src/main/java/com/example/groupb/sack_app/ScenariosActivity.java
+++ app/src/main/java/com/example/groupb/sack_app/ScenariosActivity.java
@@ -1,7 +1,9 @@
 package com.example.groupb.sack_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ScenariosActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class ScenariosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scenarios);
+    }
+
+    public void toScenario1(View view) {
+        Intent intent = new Intent(this, Scenario1MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void toScenario2(View view) {
+        Intent intent = new Intent(this, scenario2Main.class);
+        startActivity(intent);
+    }
+
+    public void toScenario3(View view) {
+        Intent intent = new Intent(this, scenario3Main.class);
+        startActivity(intent);
+    }
+
+    public void toScenario4(View view) {
+        Intent intent = new Intent(this, scenario4Main.class);
+        startActivity(intent);
     }
 }
