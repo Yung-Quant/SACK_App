9f7615411a93938b765e7b228e7c4cab1640c557
mitchcrompton <16mrc6@queensu.ca>
2018-03-22 17:08:10 -0400

    new commit

--- app/src/main/java/com/example/groupb/sack_app/Scenario1Activity.java
+++ app/src/main/java/com/example/groupb/sack_app/Scenario1Activity.java
@@ -1,15 +1,13 @@
 package com.example.groupb.sack_app;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class ScenariosActivity extends AppCompatActivity {
+public class Scenario1Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scenarios);
+        setContentView(R.layout.activity_scenario1);
     }
 }
