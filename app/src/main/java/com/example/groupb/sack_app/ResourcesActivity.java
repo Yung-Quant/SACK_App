83103fe775a244019136d62b7adc27b8e8188c57
mitchcrompton <16mrc6@queensu.ca>
2018-03-26 20:57:04 -0400

    fixed error in resources activity

--- app/src/main/java/com/example/groupb/sack_app/ResourcesActivity.java
+++ app/src/main/java/com/example/groupb/sack_app/ResourcesActivity.java
@@ -10,7 +10,7 @@ public class ResourcesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scenarios);
+        setContentView(R.layout.activity_resources);
     }
 
 }
