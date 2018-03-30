c181b9d35ebcbd1473895d4f34d77e2c9aa3fa54
mitchcrompton <16mrc6@queensu.ca>
2018-03-30 16:36:27 -0400

    resources page finished

--- app/src/main/java/com/example/groupb/sack_app/ResourcesActivity.java
+++ app/src/main/java/com/example/groupb/sack_app/ResourcesActivity.java
@@ -1,6 +1,7 @@
 package com.example.groupb.sack_app;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,32 @@ public class ResourcesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resources);
+    }
+
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+
+    public void toRainn (View view) {
+        goToUrl("https://www.rainn.org/");
+    }
+
+    public void toSACK (View view) {
+        goToUrl("http://sackingston.com/");
+    }
+
+    public void toQWC (View view) {
+        goToUrl("http://www.queensu.ca/studentwellness/home");
+    }
+
+    public void toQueens (View view) {
+        goToUrl("http://www.queensu.ca/studentaffairs/sites/webpublish.queensu.ca.vpsawww/files/files/SA%20steps%20to%20take%20March%202016.pdf");
+    }
+
+    public void toKHP (View view) {
+        goToUrl("https://kidshelpphone.ca/");
     }
 
     //Nav Bar Methods
